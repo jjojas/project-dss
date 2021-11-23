b75e8040a3690e65b2a644de7764cffa0b464743
merwinfattah <18219019@std.stei.itb.ac.id>
2021-11-24 06:51:45 +0700

    Update main.jsx

--- UI/novelrec/src/components/main.jsx
+++ UI/novelrec/src/components/main.jsx
@@ -4,20 +4,23 @@ import Button from '@mui/material/Button';
 
 
 class Main  extends React.Component {
-    state = [
-
-    ]
+    state = {
+        judulNovel : [],
+        genreNovel : []
+    }
 
 
     render() { 
         return (
         <div>
             <h1 >Sistem Rekomendasi Novel</h1>
-            <h3 >Masukkan judul novel yang pernah dibaca :</h3>
-            <TagInput tags={[]} placeholder={"ketikkan judul novel"} />
-            <h3 >Masukkan genre novel yang diingingkan :</h3>
-            <TagInput tags={[]} placeholder={"Ketikkan genre novel"} />
-            <Button variant="contained" id="tombol" color="secondary">Cari rekomendasi</Button>
+            <form>
+                <h3 >Masukkan judul novel yang pernah dibaca :</h3>
+                <TagInput tags={[]} placeholder={"ketikkan judul novel"} />
+                <h3 >Masukkan genre novel yang diingingkan :</h3>
+                <TagInput tags={[]} placeholder={"Ketikkan genre novel"} />
+                <Button variant="contained" id="tombol" color="secondary">Cari rekomendasi</Button>
+            </form>
         </div>
         );
     }
