7ae92fa6d2b8fbeb22a536a64fdd7f10cc4240c4
jjojas <justinjojas@gmail.com>
2021-11-25 18:13:19 +0700

    Add CORS

--- back-end/app.py
+++ back-end/app.py
@@ -2,9 +2,25 @@ import json
 from typing import List
 import modules.recommend as rc
 from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Book(BaseModel):
     id: str
